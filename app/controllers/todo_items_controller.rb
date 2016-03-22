8b8ded68b6a246ad1d03cf06c1460697700f7f26
Aleksandr Zakharov <human.of.rain@gmail.com>
2016-03-22 08:24:12 +0300

    Another errors

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,5 +1,5 @@
 class TodoItemsController < ApplicationController
-  before_action :set_todo_list, only: [:show, :edit, :update, :destroy, :set_todo_item]
+  before_action :set_todo_list
   before_action :set_todo_item, only: [:show, :edit, :update, :destroy]
  
 
